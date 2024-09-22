ef21d96509f5f3052567a9393d8e77b90269c2f4
dhanureddy2003 <dhanushreddy248@gmail.com>
2024-09-22 21:22:44 +0530

    env changes

--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,11 @@ import authRoutes from "./routes/auth.router.js";
 import userRoutes from "./routes/user.router.js";
 import postRoutes from "./routes/post.router.js";
 import notificationRoutes from "./routes/notification.router.js";
+import path from "path";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
-const app = express();
-app.use(express.json({limit: "10mb"}));
 dotenv.config();
-app.use(express.urlencoded({limit: "10mb", extended: true }));
-app.use(cookieParser());
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
@@ -19,11 +16,24 @@ cloudinary.config({
   api_secret: process.env.CLOUD_API_SECRET,
 });
 
+const app = express();
+const __dirname = path.resolve();
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(cookieParser());
+
 // routes
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/notifications", notificationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/vite-project/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 (async () => {
   try {
     await mongoose.connect(`${process.env.MONGO_URI}`);
